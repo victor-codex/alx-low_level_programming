97492ab8f69ecb7b1e6a8c06b58f45c873c9ee86
Victor Umeh <umehvictor28@gmail.com>
2023-08-14 05:32:23 -0700

    5-printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - the main function returns a text
+ *
+ * Return: returns 0 afer print
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+		return (0);
+}
