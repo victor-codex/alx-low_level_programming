bdc944ca31c6ad0e623286cd7ffa4f647e883d00
Victor Umeh <umehvictor28@gmail.com>
2023-08-14 22:14:27 -0700

    5-print

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: 0 when successful
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
