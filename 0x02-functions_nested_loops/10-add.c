439d971e1dacb29ead6f1afddccf42a484e89dc9
Victor Umeh <umehvictor28@gmail.com>
2023-08-16 18:52:09 -0700

    ./10-add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - adds two intergers
+ *@a: first params
+ *@b: second params
+ * Return: 0;
+ */
+
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+	return (c);
+}
