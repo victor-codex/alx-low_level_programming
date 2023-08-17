9bf18bb1787c07e7b81f42928b5b4845793c7ed5
Victor Umeh <umehvictor28@gmail.com>
2023-08-16 19:40:55 -0700

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: 0 when succesful
+ */
+
+int main(void)
+{
+	int sum = 0;
+	int i;
+
+	for (i = 1 ; i <= 1024 ; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+			sum = sum + i;
+	}
+	printf("%d\n", sum);
+}
