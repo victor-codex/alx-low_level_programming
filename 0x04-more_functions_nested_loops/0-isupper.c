82dbcb51039a5cb159fc56360f7af7cab9519168
Victor Umeh <umehvictor28@gmail.com>
2023-08-17 12:55:04 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase
+ * @c: params
+ * Return: 0;
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		_putchar("1");
+	return (0);
+}
