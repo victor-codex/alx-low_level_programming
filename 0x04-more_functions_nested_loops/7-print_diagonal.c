0e476d37c5744a83d63073e1e25524bf5e3051ab
Victor Umeh <umehvictor28@gmail.com>
2023-08-18 17:04:21 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -16,7 +16,7 @@ void print_diagonal(int n)
 	{
 		for (i = 0 ; i < n ; i++)
 		{
-			for (j = i ; j <= i ; j++)
+			for (j = 1 ; j <= i ; j++)
 				_putchar(' ');
 			_putchar('\\');
 			_putchar('\n');
