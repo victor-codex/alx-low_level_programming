1dd0caa4bc29f945bed4fc5310672e1072901b20
Victor Umeh <umehvictor28@gmail.com>
2023-08-18 17:00:49 -0700

    ./8-squares

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_square - prints square
+ * @size: size params
+ * Return: 0;
+ */
+
+void print_square(int size)
+{
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		int i, j;
+
+		for (i = 1 ; i <= size ; i++)
+		{
+			for (j = 1 ; j <= size ; j++)
+				_putchar('#');
+			_putchar('\n');
+		}
+	}
+}
