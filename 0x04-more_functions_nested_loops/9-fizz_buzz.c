59366b3fa82596f49a68b7182f8ca763653cb422
Victor Umeh <umehvictor28@gmail.com>
2023-08-18 18:01:52 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,10 +11,10 @@ int main(void)
 
 	for (i = 1 ; i <= 100 ; i++)
 	{
-		if ((i % 3 == 0) && (i % 5 == 0))
-			printf("%s", "FizzBuzz");
-		else if (i % 3 == 0)
-			printf("%s", "fizz");
+		if ((i % 3) == 0 && (i % 5) == 0)
+			printf("FizzBuzz");
+		else if ((i % 3) == 0)
+			printf("%s", "Fizz");
 		else if (i % 5 == 0)
 			printf("%s", "Buzz");
 		else
