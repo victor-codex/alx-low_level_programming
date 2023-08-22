206f07bcae3f550b423d35a8cfe193002a961066
Victor Umeh <umehvictor28@gmail.com>
2023-08-22 08:06:25 -0700

    int _strlen(char *s)

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - returns length of a string
+ * @c: parameter
+ * Return: 0;
+ */
+
+int _strlen(char *s)
+{
+	int longi = 0;
+
+	while (*s != '\0')
+	{
+		longi++;
+		s++;
+	}
+
+	return (longi);
+}
